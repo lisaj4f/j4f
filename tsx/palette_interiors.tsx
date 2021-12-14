33230730a7170c0aa782f57f36d766b582ab5e77
lisaj4f <Lisa@J4F.be>
2021-12-14 16:39:42 +0100

    aa

--- tsx/palette_interiors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="palette_interiors" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../Downloads/Modern_Interiors/Palettes/palette_interiors.png" width="43" height="45"/>
-</tileset>
